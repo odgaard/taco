b84566f806db52388596e221b4080afe5d6c6f27
Jacob Odgård Tørring <jacob.torring@gmail.com>
2023-09-25 15:15:56 +0200

    Adds preliminary singularity support

--- benchmark_runner.py
+++ benchmark_runner.py
@@ -3,63 +3,98 @@ import docker
 import argparse
 import concurrent.futures
 
-def run_docker_with_args_taco(image_name, mat_val, method_val, o_val):
-    client = docker.from_env()
+class SingularityRunner:
+    containers = []
 
-    command = [
-        '-mat', mat_val,
-        '--method', method_val,
-        '-o', o_val
-    ]
+    def terminate_containers(self):
+        for container in self.containers:
+            container.terminate()
 
-    # Define the volume mapping
-    volume_mapping = {
-        os.path.join(os.getcwd(), 'build/experiments'): {
-            'bind': '/app/build/experiments',
-            'mode': 'rw'
+    def run_container_with_args_taco(self, image_name, mat_val, method_val, o_val):
+        command = [
+            'singularity', 'run',
+            image_name,
+            '-mat', mat_val,
+            '--method', method_val,
+            '-o', o_val
+        ]
+        process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        self.containers.append(process)
+        return process
+
+    def run_container_with_args_hypermapper(self, command, image_name, json_file):
+        singularity_cmd = [
+            'singularity', 'run',
+            '--bind', f"{os.path.join(os.getcwd(), 'build/experiments')}:/app/build/experiments",
+            image_name
+        ] + command  # appending command list to singularity command
+        process = subprocess.Popen(singularity_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        self.containers.append(process)
+        return process
+
+class DockerRunner:
+    containers = []
+
+    def terminate_containers(self):
+        for container in self.containers:
+            container.stop()
+
+    def run_container_hypermapper(self, command, image_name, json_file):
+        volume_mapping = {
+            os.path.join(os.getcwd(), 'build/experiments'): {
+                'bind': '/app/build/experiments',
+                'mode': 'rw'
+            }
         }
-    }
-    container = client.containers.run(
+        client = docker.from_env()
+        container = client.containers.run(
             image_name,
             command,
             detach=True,
             network_mode="host",
             volumes=volume_mapping
-    )
-    return container
+        )
+        self.containers.append(container)
+        return container
 
-def run_docker_with_args_hypermapper(image_name, json_file):
-    client = docker.from_env()
-    command = [json_file]
-
-    volume_mapping = {
-        os.path.join(os.getcwd(), 'build/experiments'): {
-            'bind': '/app/build/experiments',
-            'mode': 'rw'
+    def run_container_taco(self, image_name, mat_val, method_val, o_val):
+        client = docker.from_env()
+        command = [
+            '-mat', mat_val,
+            '--method', method_val,
+            '-o', o_val
+        ]
+        volume_mapping = {
+            os.path.join(os.getcwd(), 'build/experiments'): {
+                'bind': '/app/build/experiments',
+                'mode': 'rw'
+            }
         }
-    }
-    container = client.containers.run(
+        container = client.containers.run(
             image_name,
             command,
             detach=True,
             network_mode="host",
             volumes=volume_mapping
-    )
-    return container
+        )
+        self.containers.append(container)
+        return container
 
 def collect_logs(container, name):
     for line in container.logs(stream=True):
         text = line.strip().decode('utf-8')
         print(f"[{name}]: {text}")
 
-def run_program(taco_image, mat, method, o, hypermapper_image, json):
+def run_program(runtime, taco_image, mat, method, o, hypermapper_image, json):
     taco_container = None
     hypermapper_container = None
 
+    runner = DockerRunner() if runtime == "docker" else SingularityRunner()
+
     try:
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            taco_future = executor.submit(run_docker_with_args_taco, taco_image, mat, method, o)
-            hypermapper_future = executor.submit(run_docker_with_args_hypermapper, hypermapper_image, json)
+            taco_future = executor.submit(runner.run_container_taco, taco_image, mat, method, o)
+            hypermapper_future = executor.submit(runner.run_container_hypermapper, [json], hypermapper_image, json)
 
         taco_container = taco_future.result()
         hypermapper_container = hypermapper_future.result()
@@ -73,14 +108,11 @@ def run_program(taco_image, mat, method, o, hypermapper_image, json):
     except Exception as e:
         print(f"Error: {e}. Stopping containers...")
     finally:
-        if hypermapper_container:
-            hypermapper_container.stop()
-        if taco_container:
-            taco_container.stop()
+        runner.terminate_containers()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run Docker images with specific commands.")
-
+    parser.add_argument("--runtime", choices=['docker', 'singularity'], required=True, help="Container runtime to use.")
     parser.add_argument("--taco-image", required=True, help="Docker image name for the taco command.")
     parser.add_argument("--mat", required=True, help="Argument for '-mat' in the taco command.")
     parser.add_argument("--method", default="random", help="Argument for '--method' in the taco command.")
@@ -91,5 +123,6 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    run_program(args.taco_image, args.mat, args.method, args.o, args.hypermapper_image, args.json)
+    run_program(args.runtime, args.taco_image, args.mat, args.method, args.o, args.hypermapper_image, args.json)
+    print("Done")
 
--- hardcoded-runner.py
+++ hardcoded-runner.py
@@ -2,14 +2,16 @@ import benchmark_runner
 
 def benchmark_run(mat, benchmark, method="random"):
     # hard-coded values
+    runtime = "docker"
     taco_image = "odgaard/bacobench:0.0.7"
     hypermapper_image = "odgaard/hypermapper-grpc:0.0.2"
 
     mat_path = f"{mat}/{mat}.mtx"
     json = f"build/experiments/outdata_{benchmark}_{mat}/{mat}/{method}/cpp_taco_{benchmark}_{method}/cpp_taco_{benchmark}_{method}_scenario.json"
 
-    benchmark_runner.run_program(taco_image, mat_path, method, benchmark, hypermapper_image, json)
+    benchmark_runner.run_program(runtime, taco_image, mat_path, method, benchmark, hypermapper_image, json)
 
 
 if __name__ == "__main__":
     benchmark_run("Goodwin_040", "SpMM")
+    #benchmark_run("Goodwin_040", "SDDMM")
